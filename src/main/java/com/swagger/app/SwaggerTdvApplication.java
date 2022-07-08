bc8e5264bd547a36be015694ad37399629899205
seba <sebaesains77@gmail.com>
2022-07-08 19:53:08 -0300

    Ultimo tp de la facu Trabajo terminadogit add .!

--- src/main/java/com/swagger/app/SwaggerTdvApplication.java
+++ src/main/java/com/swagger/app/SwaggerTdvApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeleniumTdvApplication {
+public class SwaggerTdvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeleniumTdvApplication.class, args);
+		SpringApplication.run(SwaggerTdvApplication.class, args);
 	}
 
 }
